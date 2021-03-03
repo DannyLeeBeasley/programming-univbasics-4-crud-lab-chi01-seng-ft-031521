f4c9c34dcbe20caba75e253dec37c62dca4d3e0b
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-03 21:55:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["John Bonham", "John Paul Jones", "Jimmy Page", "Robert Plant"] << "Me"
+  led_zeppelin << "Me"
 end
 
 def add_element_to_start_of_array(array, element)

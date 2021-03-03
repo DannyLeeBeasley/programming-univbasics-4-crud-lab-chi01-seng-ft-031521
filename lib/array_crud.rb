33952c1731e1ad795d9a15a4bfdccd0f4a62ee30
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-03 21:45:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  led_zeppelin.push("Me")
+  led_zeppelin.push(led_zeppelin, "Me")
 end
 
 def add_element_to_start_of_array(array, element)
